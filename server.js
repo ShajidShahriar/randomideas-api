681d43d7128bb2960d7e7ddcf61db423e4ca7951
shajid shahriar <shajidsr12@gmail.com>
2025-11-27 18:48:38 +0600

    initial commit

--- server.js
+++ server.js
@@ -1,21 +1,48 @@
-const express = require('express');
-require('dotenv').config();
-const port = process.env.PORT || 5000;
-const connectDB = require('./config/db');
+const express  = require('express')
+const port = 5001
 
-connectDB();
+const app = express()
 
-const app = express();
+const ideas = [
+  {
+    id: 1,
+    text: 'Please add a dark mode button',
+    tag: 'technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'We need more tutorials on AI',
+    tag: 'education',
+    username: 'BruceBanner',
+    date: '2023-01-02',
+  },
+  {
+    id: 3,
+    text: 'Free coffee for all developers',
+    tag: 'lifestyle',
+    username: 'PeterParker',
+    date: '2023-01-02',
+  },
+];
 
-// Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.get('/',(req,res) =>{
+  res.json({ message: 'welcome to my twitter app ' })
+})
+//get all ideas
+app.get('/api/ideas',(req,res) =>{
+  res.json({success: true ,data: ideas })
+})
+app.get('/api/ideas/:id',(req,res) =>{
+  const idea = ideas.find((idea) => idea.id === +req.params.id)
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the RandomIdeas API' });
-});
+  if(!idea){
+    return res.status(404).json({ success: false , error: 'Resource not found'})
+  }
+  res.json({success: true ,data: idea })
+})
 
-const ideasRouter = require('./routes/ideas');
-app.use('/api/ideas', ideasRouter);
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+app.listen(port,() => console.log(`server listening on port ${port}`))
