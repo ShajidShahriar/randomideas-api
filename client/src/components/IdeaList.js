3e14de309ba03e7bac790c3ed334d6cf6ee39688
shajid shahriar <shajidsr12@gmail.com>
2025-12-20 21:28:49 +0600

    add delete functionality

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,46 +1,71 @@
 import IdeasApi from "../services/ideasApi"
-class IdeaList{
-    constructor(){
-        this._ideaListEl = document.querySelector('#idea-list')
-        this._ideas = []
-        this.getIdeas()
-    this._validTags = new Set()
-    this._validTags.add('technology')
-    this._validTags.add('software')
-    this._validTags.add('business')
-    this._validTags.add('education')
-    this._validTags.add('health')
-    this._validTags.add('inventions')
+class IdeaList {
+  constructor() {
+    this._ideaListEl = document.querySelector("#idea-list");
+    this._ideas = [];
+    this.getIdeas();
+    this._validTags = new Set();
+    this._validTags.add("technology");
+    this._validTags.add("software");
+    this._validTags.add("business");
+    this._validTags.add("education");
+    this._validTags.add("health");
+    this._validTags.add("inventions");
+  }
+
+  addEventListeners() {
+    this._ideaListEl.addEventListener("click", (e) => {
+      if (e.target.classList.contains("fa-times")) {
+        e.stopImmediatePropagation();
+        const ideaID = e.target.parentElement.parentElement.dataset.id;
+        console.log(ideaID);
+        this.deleteidea(ideaID);
+      }
+    });
+  }
+  async getIdeas() {
+    try {
+      const res = await IdeasApi.getIdeas();
+      this._ideas = res.data.data;
+      this.render();
+    } catch (error) {
+      console.log(error);
     }
-    async getIdeas(){
-        try {
-            const res = await IdeasApi.getIdeas()
-            this._ideas = res.data.data
-            this.render()
-        } catch (error) {
-            console.log(error)           
-        }
+  }
+  async deleteidea(ideaID) {
+    try {
+      //delete from server
+      const res = await IdeasApi.deleteIdea(ideaID);
+      this._ideas.filter((idea) => idea._id !== ideaID);
+      this.getIdeas();
+    } catch (error) {
+      alert("you can not delete this resource");
     }
-    addIdeaToList(idea){
-        this._ideas.push(idea)
-        this.render()
+  }
+  addIdeaToList(idea) {
+    this._ideas.push(idea);
+    this.render();
+  }
+  _getTagClass(tag) {
+    tag = tag.toLowerCase();
+    let tagClass = "";
+    if (this._validTags.has(tag)) {
+      tagClass = `tag-${tag}`;
+    } else {
+      tagClass = "";
     }
-    _getTagClass(tag){
-        tag = tag.toLowerCase()
-        let tagClass = ''
-        if(this._validTags.has(tag)){
-            tagClass = `tag-${tag}`
-        }else{
-            tagClass = ''
-        }
-        return tagClass
-    }
-    render(){
-        this._ideaListEl.innerHTML = this._ideas.map((idea) =>{
-            const tagClass = this._getTagClass(idea.tag)
-            return `
-            <div class="card">
-          <button class="delete"><i class="fas fa-times"></i></button>
+    return tagClass;
+  }
+  render() {
+    this._ideaListEl.innerHTML = this._ideas.map((idea) => {
+      const tagClass = this._getTagClass(idea.tag);
+      const deleteBtn =
+        idea.username === localStorage.getItem("username")
+          ? `<button class="delete"><i class="fas fa-times"></i></button`
+          : "";
+      return `
+            <div class="card" data-id="${idea._id}">
+            ${deleteBtn}
           <h3>
             ${idea.text}
           </h3>
@@ -49,11 +74,10 @@ class IdeaList{
             Posted on <span class="date">${idea.date}</span> by
             <span class="author">${idea.username}</span>
           </p>
-        </div>`
-        })
-    }
-
-
+        </div>`;
+    });
+    this.addEventListeners();
+  }
 }
 
 export default IdeaList
